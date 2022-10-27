93c3559c5f8bc734ba7c84f9d970f804a03f13e0
Veratr <116810899+Veratr@users.noreply.github.com>
2022-10-27 16:40:31 +0300

    Add files via upload
    задание 4(егэ 2) от 27.10

---
+++ 27.10.4.py
@@ -0,0 +1,7 @@
+print(" z, x, y, w")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if (not((x or y) <= (z and w)) and (x <=w)):
+                    print(z, x, y, w)
